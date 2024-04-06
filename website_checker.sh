f58efb648a27c59fbbe7b18d5ff50f2fbf84cc89
yevhenporohniaviy <porohniaviy.yevheni@gmail.com>
2024-04-06 08:59:07 +0300

    fix:bash script

--- website_checker.sh
+++ website_checker.sh
@@ -10,22 +10,19 @@ websites=(
 # Ім'я файлу логів
 log_file="website_status.log"
 
-# Перевірка доступності кожного сайту
-for site in "${websites[@]}"; do
-    # Виконання HTTP GET запиту до сайту і перевірка HTTP статус-коду
-    response=$(curl -Is $site | head -n 1)
-    if [[ $response == *"200 OK"* ]]; then
-        status="UP"
-    else
-        status="DOWN"
-    fi
-    
-    # Запис результату перевірки в файл логів
-    echo "$site is $status" >> $log_file
-    
-    # Виведення результату на екран
-    echo "$site is $status"
-done
+# Чистемо файл логів
+> "$log_file"
 
-# Повідомлення про завершення запису результатів у файл логів
-echo "Результати записано у файл $log_file"
+# Перевірка доступності кожного сайту
+for site in "${websites[@]}"
+do
+    
+    if curl -o /dev/null -s -L --head -w "%{http_code}\n" "$site" | grep 200 > /dev/null
+    then
+        echo "$site is UP" >> "$log_file"
+        echo "$site is UP"
+    else
+        echo "$site is DOWN" >> "$log_file"
+        echo "$site is DOWN"
+    fi
+done
